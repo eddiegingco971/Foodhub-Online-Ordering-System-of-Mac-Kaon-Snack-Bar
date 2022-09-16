af1ec587f1dc050fc46ed2ea4fef5aee32966f44
Eddie Gingco <gingcoeddie971@gmail.com>
2022-09-16 15:49:17 +0800

    updating-adminlte

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,5 @@
 @extends('layouts.app')
+
 @include('admin.navbar')
 
-@section('content')
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
-            
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,37 +9,33 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-     <!-- Tell the browser to be responsive to screen width -->
-     <meta name="viewport" content="width=device-width, initial-scale=1">
-     <!-- Font Awesome -->
-     <link rel="stylesheet" href="{{asset('/assets')}}/plugins/fontawesome-free/css/all.min.css">
-     <!-- Ionicons -->
-     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-     <!-- Tempusdominus Bbootstrap 4 -->
-     <link rel="stylesheet" href="{{asset('/assets')}}/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-     <!-- iCheck -->
-     <link rel="stylesheet" href="{{asset('/assets')}}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-     <!-- JQVMap -->
-     <link rel="stylesheet" href="{{asset('/assets')}}/plugins/jqvmap/jqvmap.min.css">
-     <!-- Theme style -->
-     <link rel="stylesheet" href="{{asset('/assets')}}/dist/css/adminlte.min.css">
-     <!-- overlayScrollbars -->
-     <link rel="stylesheet" href="{{asset('/assets')}}/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-     <!-- Daterange picker -->
-     <link rel="stylesheet" href="{{asset('/assets')}}/plugins/daterangepicker/daterangepicker.css">
-     <!-- summernote -->
-     <link rel="stylesheet" href="{{asset('/assets')}}/plugins/summernote/summernote-bs4.css">
-     <!-- Google Font: Source Sans Pro -->
-     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+   <!-- Google Font: Source Sans Pro -->
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+   <!-- Font Awesome -->
+   <link rel="stylesheet" href="{{asset('/assets')}}/plugins/fontawesome-free/css/all.min.css">
+   <!-- Ionicons -->
+   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+   <!-- Tempusdominus Bootstrap 4 -->
+   <link rel="stylesheet" href="{{asset('/assets')}}/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+   <!-- iCheck -->
+   <link rel="stylesheet" href="{{asset('/assets')}}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+   <!-- JQVMap -->
+   <link rel="stylesheet" href="{{asset('/assets')}}/plugins/jqvmap/jqvmap.min.css">
+   <!-- Theme style -->
+   <link rel="stylesheet" href="{{asset('/assets')}}/dist/css/adminlte.min.css">
+   <!-- overlayScrollbars -->
+   <link rel="stylesheet" href="{{asset('/assets')}}/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+   <!-- Daterange picker -->
+   <link rel="stylesheet" href="{{asset('/assets')}}/plugins/daterangepicker/daterangepicker.css">
+   <!-- summernote -->
+   <link rel="stylesheet" href="{{asset('/assets')}}/plugins/summernote/summernote-bs4.min.css">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body>
+<body class="wrapper">
+  
   <div class="hold-transition sidebar-mini">
     <div id="app">
       <main class="py-4">
@@ -47,6 +43,7 @@
         </main>
     </div>
   </div>
+
 
 <!-- jQuery -->
 <script src="{{asset('/assets')}}/plugins/jquery/jquery.min.js"></script>
@@ -78,9 +75,9 @@
 <script src="{{asset('/assets')}}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
 <script src="{{asset('/assets')}}/dist/js/adminlte.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{asset('/assets')}}/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="{{asset('/assets')}}/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{asset('/assets')}}/dist/js/pages/dashboard.js"></script>
 </body>
 </html>
