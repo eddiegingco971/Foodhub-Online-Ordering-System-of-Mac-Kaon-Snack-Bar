1ee920650a3fcfb9199fe4322fae88909c202e0d
Eddie Gingco <gingcoeddie971@gmail.com>
2022-09-21 08:30:32 +0800

    update2-admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
     </div>
    
 
-
+    <script src="{{asset('/assets')}}/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="{{asset('/assets')}}/js/scripts.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
     <script src="{{asset('/assets')}}/demo/chart-area-demo.js"></script>
