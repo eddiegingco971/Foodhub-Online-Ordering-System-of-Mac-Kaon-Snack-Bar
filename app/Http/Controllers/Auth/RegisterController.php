10447db20e48ac7d7cc45af60bb5e699357d1798
Eddie Gingco <gingcoeddie971@gmail.com>
2022-11-03 09:33:20 +0800

    updated

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -33,13 +33,13 @@ class RegisterController extends Controller
     // protected $redirectTo = RouteServiceProvider::HOME;
     protected function redirectTo(){
         if(Auth::user()-> user_type == 'admin'){
-            return '/';
+            return '/home';
 
         }else if(Auth::user()-> user_type == 'staff'){
-            return '/';
+            return '/staff';
 
         }else{
-            return '/';
+            return '/user';
         }
       }
 
