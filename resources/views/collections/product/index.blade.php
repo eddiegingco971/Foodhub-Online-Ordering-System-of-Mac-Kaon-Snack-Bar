8ebb40041b40d23a8f298d6c024b08ea79657060
Eddie Gingco <gingcoeddie971@gmail.com>
2022-11-16 13:12:09 +0800

    design

--- resources/views/collections/product/index.blade.php
+++ resources/views/collections/product/index.blade.php
@@ -14,13 +14,19 @@
                 <div class="product-card">
                     <a href="{{url('/collections/'.$product->categories->category_name.'/'.$product->product_name)}}">
                         <div class="product-card-img">
+                            @if ($product->status == 'active')
+                                <label class="stock bg-success">Available</label>
+                            @else
+                                <label class="stock bg-danger">Not Available</label>
+                            @endif
                             <img src="{{asset('dist/img/product/'.$product->image)}}" class="w-100" alt="Product Image">
                         </div>
                         <div class="product-card-body">
-                            <h5>{{$product->product_name}}</h5>
+                            <h5 class="text-center">{{$product->product_name}}</h5>
                         </div>
-                        <div class="col-md-6">
-                            <p>{{$product->description}}</p>
+                        <div class="col-md-12 text-sm p-2" style="height: 100px">
+                            <p class="text-sm">Description</p>
+                            <p class="text-justify text-sm">{{$product->description}}</p>
                         </div>
                     </a>
                 </div>
