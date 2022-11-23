62c444e01afb623122e2313459916c9d33151303
Eddie Gingco <gingcoeddie971@gmail.com>
2022-11-23 09:56:03 +0800

    updated

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -37,20 +37,31 @@ class CartController extends Controller
     //        'status' => 'required',
     //    ]);
     // $products = DB::table('products')->where('status', 'active')->get();
-        Cart::with(['products'])->create([
-            'product_id' => $request->product_id,
-            'user_id' => auth()->user()->id,
-        //    'user_id' => $request->user_id,
-            'quantity' => $request->quantity,
-            'total_amount' =>  $request->price*$request->quantity,
-            'status' => $request->status,
-        ]);
+        $cart = Cart::with('products')->where('user_id', auth()->user()->id)->where('product_id', $request->product_id)->first();
+        if(isset($cart)){
+            $quantity = $request->quantity + $cart->quantity;
+            $cart->update([
+                'quantity' => $quantity,
+                'total_amount' => $cart->price*$quantity,
+               'status' => $request->status,
+            ]);
+        }
+        else{
+            Cart::with(['products'])->create([
+                'product_id' => $request->product_id,
+                'user_id' => auth()->user()->id,
+            //    'user_id' => $request->user_id,
+                'quantity' => $request->quantity,
+                'total_amount' =>  $request->price*$request->quantity,
+                'status' => $request->status,
+            ]);
+        }
+
 
        if(Cart::where('user_id', auth()->user()->id)->with('products' , 'product_id')->exists()) {
-            return back()->with('error', 'Product is already Added');
-       }else{
-
             return back()->with('status', 'Added Product Successfully');
+       }else{
+            return back()->with('error', 'Something went wrong');
        }
 
    }
